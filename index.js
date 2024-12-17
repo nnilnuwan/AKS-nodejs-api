2a33e0a183e8ab2ccc729bc90ab6036a5b152577
Ubuntu <nilnuwan@AKS-JUMP-VM.3isiucapw3tedgyjfx35p5xhhd.bx.internal.cloudapp.net>
2024-12-17 05:37:37 +0000

    index.js api commit ci/cd1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Simple Node.js API');
+  res.send('Welcome to the Simple Node.js API2002');
 });
 
 app.get('/api', (req, res) => {
