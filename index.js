42845e30edef41726dd0d70079a5b5592dcc3267
Ubuntu <nilnuwan@AKS-JUMP-VM.3isiucapw3tedgyjfx35p5xhhd.bx.internal.cloudapp.net>
2024-12-17 05:20:20 +0000

    hellow world commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Simple Node.js API20022002!');
+  res.send('Welcome to the Simple Node.js API hellow world!');
 });
 
 app.get('/api', (req, res) => {
