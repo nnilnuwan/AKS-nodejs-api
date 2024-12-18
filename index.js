2c5208ce36edcf522cb567b42d3a7d5139e13cb5
Ubuntu <nilnuwan@AKS-JUMP-VM.3isiucapw3tedgyjfx35p5xhhd.bx.internal.cloudapp.net>
2024-12-18 07:11:35 +0000

    all pipeline files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Simple Node.js - by nilnuwan sankalpa 2002');
+  res.send('Welcome to the Simple Node.js - by nilnuwan sankalpa 2002 02');
 });
 
 app.get('/api', (req, res) => {
