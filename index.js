0487652475e16898799b4e3a677040805f7ae983
Ubuntu <nilnuwan@AKS-JUMP-VM.3isiucapw3tedgyjfx35p5xhhd.bx.internal.cloudapp.net>
2024-12-20 04:09:18 +0000

    version 1.0.9

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Simple Node.js API! 20020429');
+  res.send('Welcome to the Simple Node.js API! 2002042');
 });
 
 app.get('/api', (req, res) => {
